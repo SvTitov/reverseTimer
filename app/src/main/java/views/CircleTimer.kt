f782ebd4a0001a6c72e2e49ff125601675e871c0
Svyatoslav Titov <slava.titoff@yandex.ru>
2020-10-25 20:05:55 +0400

    Edited moving circle

--- app/src/main/java/views/CircleTimer.kt
+++ app/src/main/java/views/CircleTimer.kt
@@ -1,14 +1,14 @@
 package views
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 import com.example.watchtimer.R
+import kotlin.math.cos
+import kotlin.math.sin
+
 
 class CircleTimer : View {
 
@@ -16,6 +16,7 @@ class CircleTimer : View {
     private var cWeight: Int = 0
     private var emptyText: String = "0:00"
     private var paint: Paint = Paint()
+    private var angle: Float = 270f
 
     var text: String? = null
         get(){
@@ -44,8 +45,51 @@ class CircleTimer : View {
 
     override fun draw(canvas: Canvas?) {
         super.draw(canvas)
+        drawShadow(canvas)
         drawCircle(canvas)
+        drawMovingCircle(canvas)
         drawText(canvas)
+    }
+
+    private fun drawShadow(canvas: Canvas?) {
+        var uWidth = (width / 2).toFloat()
+        var uHeight = (height / 2).toFloat()
+        var radius = uWidth + 15
+
+        val gradient: RadialGradient = RadialGradient(
+            uWidth, uHeight, radius , -0x1,
+            -0x1000000, Shader.TileMode.CLAMP
+        )
+
+        val p = Paint()
+        p.isDither = true
+        p.shader = gradient
+        canvas!!.drawCircle(uWidth, uHeight, radius, p)
+    }
+
+    private fun drawMovingCircle(canvas: Canvas?) {
+        //https://www.khanacademy.org/math/algebra2/x2ec2f6f830c9fb89:trig/x2ec2f6f830c9fb89:unit-circle/a/trig-unit-circle-review
+
+        var radius = 20f
+
+        var ofRadius = width / 2 - 20
+
+        var pos = getPosition(
+            PointF((width / 2).toFloat(), (height / 2).toFloat()),
+            ofRadius.toFloat(),
+            angle
+        )
+
+        paint.color = Color.GREEN
+
+        canvas?.drawCircle(pos!!.x, pos!!.y, radius, paint)
+    }
+
+    private fun getPosition(center: PointF, radius: Float, angle: Float): PointF? {
+        return PointF(
+            (center.x + radius * cos(Math.toRadians(angle.toDouble()))).toFloat(),
+            (center.y + radius * sin(Math.toRadians(angle.toDouble()))).toFloat()
+        )
     }
 
     private fun drawText(canvas: Canvas?) {
@@ -56,9 +100,11 @@ class CircleTimer : View {
 
         setCustomFont()
 
-        canvas?.drawText(text!!, (width /2).toFloat() - size.width() / 2,
-            (height/2).toFloat(),
-            paint)
+        canvas?.drawText(
+            text!!, (width / 2).toFloat() - size.width() / 2,
+            (height / 2).toFloat(),
+            paint
+        )
     }
 
     private fun setCustomFont() {
@@ -68,22 +114,15 @@ class CircleTimer : View {
 
     private fun getTextSize(text: String, paint: Paint): Rect {
         var rect = Rect()
-
-        paint.getTextBounds(text,0, text.length, rect)
-
+        paint.getTextBounds(text, 0, text.length, rect)
         return rect
     }
 
     private fun drawCircle(canvas: Canvas?) {
-
         var centerX = width / 2
         var centerY = height / 2
 
-        var radius = width / 2
-
-        paint.color = Color.BLACK
-
-        canvas?.drawPaint(paint)
+        var radius = (width / 2) - 20
 
         paint.color = Color.RED
 
